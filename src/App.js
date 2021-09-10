0f49ad5da16697273992e3eb6bff12f3d729bcac
Kravchuk Ilya <Kravchuk00700@gmail.com>
2021-09-10 09:33:33 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target='_blank'
                     rel='noopener noreferrer'
                 >
-                    Привет
+                    erererererererer
                 </a>
             </header>
         </div>
