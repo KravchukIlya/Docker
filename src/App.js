3dbbd43bbab6ec5d8b9afbbb65375c8ba5dc6a3c
Kravchuk Ilya <Kravchuk00700@gmail.com>
2021-09-12 23:17:47 +0200

    qqqqqqq

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target='_blank'
                     rel='noopener noreferrer'
                 >
-                    Привет Docker
+                    Привет Dockereeeeeeee
                 </a>
             </header>
         </div>
