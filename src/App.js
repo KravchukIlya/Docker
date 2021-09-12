43ddf72365594e6f34ef51aeaa51a766e57975b6
Kravchuk Ilya <Kravchuk00700@gmail.com>
2021-09-12 23:10:22 +0200

    кккккк

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target='_blank'
                     rel='noopener noreferrer'
                 >
-                    erererererererer
+                    Привет
                 </a>
             </header>
         </div>
